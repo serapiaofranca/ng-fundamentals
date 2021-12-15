e002fd5b4bba97d730981927809b16637cf3fb5f
Rogerio A Serapiao <rserapa@gmail.com>
2021-12-15 20:47:27 -0300

    create to single a parent components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list-components';
+import { EventsAppComponent } from './events-app.component'
+import { EventsListComponent } from './events/events-list-components'
+import { EventThumbnailComponent } from './events/event-thumbnail.component'
 
 @NgModule({
 
@@ -11,7 +12,8 @@ import { EventsListComponent } from './events/events-list-components';
   ],
   declarations: [
     EventsAppComponent,
-    EventsListComponent
+    EventsListComponent,
+    EventThumbnailComponent
   ],
   bootstrap: [EventsAppComponent]
 })
