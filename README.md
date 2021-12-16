529927fa1f47613567a14f9219b17d4fe6ef7793
Rogerio A Serapiao <rserapa@gmail.com>
2021-12-16 12:28:14 -0300

    create a reusable service toastr and examples with global variables

--- README.md
+++ README.md
@@ -25,3 +25,12 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+--- add ---
+## Decorator: toastr 
+```
+npm install toastr --save
+```
+ import in 'angular.json' :: 
+  "styles": ["node-modules/toastr/build/toastr.min.css"]
+  "scripts": ["node-modules/toastr/build/toastr.min.js"]
--- src/app/events/events-list-components.ts
+++ src/app/events/events-list-components.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { EventService } from './shared/event.service'
+import { ToastrService} from '../common/toastr.service'
+declare let toastr
 
 @Component({
   selector: 'events-list',
@@ -9,19 +11,23 @@ import { EventService } from './shared/event.service'
     <hr />
     <div class="row">
       <div *ngFor="let event of events" class="col-md-5">
-        <event-thumbnail [event] = "event"></event-thumbnail>
-        </div>
+        <event-thumbnail (click)="handleThumbnailClick(event.name)" [event] ="event"></event-thumbnail>
+      </div>
     </div>
   </div>
   `
 })
 export class EventsListComponent implements OnInit{
   events:any[]
-  constructor(private eventService: EventService) {
+  constructor(private eventService: EventService, private toastr:ToastrService) {
 
   }
 
   ngOnInit() {
     this.events = this.eventService.getEvents()
+  }
+
+  handleThumbnailClick(eventName) {
+    this.toastr.success(eventName)
   }
 }
