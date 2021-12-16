60b8b58e02cfde9867a8ce239fd9cb607172567a
Rogerio A Serapiao <rserapa@gmail.com>
2021-12-16 09:07:21 -0300

    style for *ngFor - grid elements - col-md-5

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -13,7 +13,12 @@ import { Component, Input} from '@angular/core'
       <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
     </div>
   </div>
-  `
+  `,
+  styles: [`
+    .thumbnail { min-height: 210px; }
+    .pad-left { margin-left: 10px; }
+    .well div { color: #bbb; }
+  `]
 })
 export class EventThumbnailComponent {
   @Input() event:any
