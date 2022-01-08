93628cb1392975780a89a9d14a0d4e6e6335ec8f
Rogerio A Serapiao <rserapa@gmail.com>
2022-01-08 16:52:35 -0300

    add routes and auth to login user

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -1,4 +1,6 @@
-import { Component} from '@angular/core'
+import { Component } from '@angular/core'
+import { AuthService } from './auth.service'
+import { Router } from '@angular/router'
 
 @Component({
   templateUrl: './login.component.html'
@@ -7,8 +9,16 @@ import { Component} from '@angular/core'
 export class LoginComonent {
   userName
   password
+  constructor ( private authService: AuthService, private router: Router) {
+
+  }
 
   login(formValues) {
-    console.log(formValues)
+    this.authService.loginUser ( formValues.userName, formValues.password)
+    this.router.navigate(['events'])
+  }
+
+  cancel() {
+    this.router.navigate(['events'])
   }
 }
