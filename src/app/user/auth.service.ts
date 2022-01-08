7b6afeda9497fec6866ea0e0d40be0dc19521300
Rogerio A Serapiao <rserapa@gmail.com>
2022-01-08 18:46:46 -0300

    create a reative form - profile

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core"
+import { first } from "rxjs/operators";
 import { IUser } from "./user.model"
 
 @Injectable()
@@ -16,4 +17,10 @@ export class AuthService {
   isAuthenticated() {
     return !!this.currentUser;
   }
+
+  updateCurrentUser( firstName: string, lastName: string){
+    this.currentUser.firstName = firstName
+    this.currentUser.lastName = lastName
+  }
+
 }
