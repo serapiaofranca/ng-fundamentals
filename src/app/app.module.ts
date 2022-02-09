a10cb12bf80b7b9b24fbfdec10edea87a57b6cf5
Rogerio A Serapiao <rserapa@gmail.com>
2022-02-09 11:38:35 -0300

    content projection component - sessions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser'
 import { RouterModule } from '@angular/router'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
-import {
+import { EventsAppComponent } from './events-app.component'
+import {EventService,
   EventsListComponent,
   EventThumbnailComponent,
-  EventService,
   EventDetailsComponent,
   CreateEventComponent,
   EventRouteActivator,
@@ -15,9 +15,10 @@ import {
   SessionListComponent
 } from './events/index'
 
-import { EventsAppComponent } from './events-app.component'
 import { NavBarComponent } from './nav/navbar.component'
 import { ToastrService } from './common/toastr.service'
+import { CollapsibleWellComponent } from
+  './common/collapsible-well.component'
 import { appRoutes } from './routes'
 import { Error404Component } from './errors/404.component'
 import { AuthService } from './user/auth.service'
@@ -38,7 +39,8 @@ import { AuthService } from './user/auth.service'
     CreateEventComponent,
     Error404Component,
     CreateSessionComponent,
-    SessionListComponent
+    SessionListComponent,
+    CollapsibleWellComponent
   ],
   providers: [
     EventService,
